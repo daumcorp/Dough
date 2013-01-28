3af5d58e9e5822e9582086cb0c9b84b4ae8b301b
조규태 <oigil@daumcorp.com>
2013-01-28 15:33:08 +0900

    불필요한 파일들 삭제.

--- view/common/footer.php
+++
@@ -1,8 +0,0 @@
-		<div class="wrap_foot">
-			<span class="line_break"></span>
-			2012 &copy; Daum Communications 웹표준기술팀 &nbsp;&nbsp; | &nbsp;&nbsp; 오류 및 문의 : 조규태 (oigil@daumcorp.com)
-		</div>
-		
-		<script>
-		getLog("<?=$page?>");
-		</script>
