6d4ee55a1668d9e6255e4f7b176122946a570766
조규태 <oigil@daumcorp.com>
2013-02-22 18:01:05 +0900

    dough 0.2.0 dough-responsible 0.2.0 dough-script 0.2.0 배포

---
+++ plug-in/dough-script/dough-script-0.2.0.js
@@ -0,0 +1,285 @@
+(function($){
+
+	
+	/*
+	 * $.tab
+	 * 
+	 *  - "data-tab-event" 속성을 가진 .tab_bar 엘리먼트를 대상으로 적용된다.
+	 *  - event는 "click"과 "mouseover" 두가지를 지원한다.
+	 */
+	
+	$.fn.tab = function(options){
+		var _options = {
+			trigger: (options && options.trigger) ? options.trigger : "click"
+		};
+		this.each(function(){
+			var _tab = this;
+			this._targetId = [];
+			this._eventTrigger = ($(this).attr("data-tab-event")) ? $(this).attr("data-tab-event").toLowerCase() : _options.trigger;
+			
+			$(this).find("> li > a").each(function(){
+				var thisId = $(this).attr("href");
+				if(thisId.match(/^#/)){
+					var eventHandler = function(e){
+						e.preventDefault();
+						$(_tab).find("li.on").removeClass("on");
+						$(e.target).parents("li").addClass("on");
+						$(_tab._targetId.join(", ")).not(thisId).hide();
+						$(thisId).show();
+					};
+					
+					switch(_tab._eventTrigger){
+						case "click": 
+							$(this).click(function(e){
+								eventHandler(e);
+							});
+							break;
+						case "mouseover": 
+							$(this).mouseover(function(e){
+								eventHandler(e);
+							});
+							$(this).click(function(e){
+								e.preventDefault();
+							});
+							break;
+						default:
+							break;
+					}
+
+					_tab._targetId.push(thisId);
+				};
+			});
+		});
+	};
+	
+	
+	/*
+	 * $.tip (툴팁 이벤트)
+	 * 
+	 *  - "data-tip-event"과 title 속성을 가진 모든 엘리먼트에 적용된다.
+	 *  - event는 "click"과 "mouseover" 두가지를 지원한다.
+	 *  - options : css | trigger
+	 */
+	$.fn.tip = function(options){
+		var _options = {
+			css: (options && options.css) ? options.css : null,
+			trigger: (options && options.trigger) ? options.trigger : "mouseover"
+		};
+		this.each(function(i){
+			this._tipBox = null;
+			this._tipText = $(this).attr("title");
+			this._tipRotate = parseInt($(this).attr("data-tip-rotate"));
+			this._eventTrigger = ($(this).attr("data-tip-event")) ? $(this).attr("data-tip-event").toLowerCase() : _options.trigger;
+			
+			this.eventOnHandler = function(e){
+				e.preventDefault();
+				if(this._tipBox == null){
+					var _tipBox = $(document.createElement("span"));
+					_tipBox.html(this._tipText).addClass("tip_box");
+					if(this._tipRotate){
+						_tipBox.css({
+							"-webkitTransform": "rotate(" + this._tipRotate + "deg)",
+							"-mozTransform": "rotate(" + this._tipRotate + "deg)",
+							"-msTransform": "rotate(" + this._tipRotate + "deg)",
+							"transform": "rotate(" + this._tipRotate + "deg)",
+							"-webkitTransformOrigin": "0 100%",
+							"-mozTransformOrigin": "0 100%",
+							"-msTransformOrigin": "0 100%",
+							"transformOrigin": "0 100%"
+						})
+					};
+					if(_options.css != null){
+						_tipBox.css(_options.css);
+					};
+					
+					this._tipBox = _tipBox;
+				};
+				$(this).removeAttr("title");
+				this._tipBox.insertAfter(this).css({
+					"marginTop": (this._tipBox.outerHeight()+3) * -1
+				})
+			};
+			
+			this.eventOffHandler = function(){
+				if(this._tipBox != null){
+					this._tipBox.remove();
+					$(this).attr("title", this._tipText);
+				};
+			};
+			
+			switch(this._eventTrigger){
+				case "mouseover": 
+					$(this).mouseover(function(e){
+						this.eventOnHandler(e);
+					});
+					$(this).mouseout(function(e){
+						this.eventOffHandler(e);
+					});
+					break;
+				case "focus": 
+					$(this).focus(function(e){
+						this.eventOnHandler(e);
+					});
+					$(this).blur(function(e){
+						this.eventOffHandler(e);
+					});
+					break;
+				default:
+					break;
+			}
+		})
+	};
+
+	/*
+	 * $.pop (팝업이벤트)
+	 * 셀렉트된 요소를 css3 transition 애니메이션으로 팝업시킨다.
+	 * options - css | callback 
+	 */
+	$.fn.pop = function(options){
+		var _options = {
+				css: (options != undefined && options.css) ? options.css : null,
+				callback: (options != undefined && options.callback) ? options.callback : null
+			},
+			_pop = this;
+		this._dimmed = function(){
+			if(!$("#_popDimmed").length){
+				var dimmed = $(document.createElement("div"));
+				dimmed.attr("id", "_popDimmed").css({
+					"position": "fixed",
+					"zIndex": "100",
+					"top": "0",
+					"left": "0",
+					"width": "100%",
+					"height": "100%",
+					"background": "#000",
+					"opacity": "0.5"
+				});
+				dimmed.insertAfter(this);
+				return dimmed;
+			}else{
+				return $("#_popDimmed");
+			}
+		};
+		
+		this._open = function(){
+			var e = e || window.event;
+			(e.preventDefault) ? e.preventDefault : e.returnValue = false;
+			this._dimmed().show();
+			$(this).show();
+			
+			setTimeout(function(el){
+				$(el).css({
+					"-webkitTransform": "scale(1)",
+					"-mozTransform": "scale(1)",
+					"-msTransform": "scale(1)"
+				});
+				if(_options.css != null){
+					$(el).css(_options.css);
+				};
+			},50, this);
+		};
+		
+		this._close = function(callback){
+			var e = e || window.event;
+			(e.preventDefault) ? e.preventDefault : e.returnValue = false;
+			$(this).css({
+				"-webkitTransform": "scale(0)",
+				"-mozTransform": "scale(0)",
+				"-msTransform": "scale(0)"
+			});
+			setTimeout(function(){
+				_pop.hide();
+				_pop._dimmed().remove();
+				if(callback != null){
+					callback();
+				};
+				delete _options.css;
+				delete _options.callback;
+				
+			}, 300);
+		};
+		
+		$(this).css({
+			"position": "fixed",
+			"zIndex": "11000",
+			"top": "50%",
+			"left": "50%",
+			"border": "none",
+			"-webkitTransform": "scale(0)",
+			"-mozTransform": "scale(0)",
+			"-msTransform": "scale(0)",
+			"-webkitTransition": "-webkit-transform 0.3s",
+			"-mozTransition": "-moz-transform 0.3s",
+			"-msTransition": "-ms-transform 0.3s",
+			"margin": "0",
+			"marginLeft": ($(this).outerWidth()/2) * -1,
+			"marginTop": ($(this).outerHeight()/2) * -1
+		});
+
+		this._open();
+		
+		$(this).find("*[data-pop-close='true']").on("click", {"pop": this}, function(e){
+			var pop = e.data.pop;
+			pop._close(_options.callback);
+		});
+		
+	};
+
+	/*
+	 * $.initResponsibleNav 
+	 * 반응형 CSS 모듈 적용했을 시 작동하는 스크립트입니다.
+	 * 화면사이즈가 작아졌을 때 모바일에 적합하게 변경시켜줍니다.
+	 */
+	$.initResponsibleNav = function(){
+		if($("body.responsible").length > 0){
+			var _isNavChanged = false,
+				navList = $(".nav_top > ul").eq(0);
+			var render = function(callback){
+				if($(window).width() < 801 && !_isNavChanged){
+					var navListWrapper = navList.wrap("<div id='navListWrapper'/>"),
+						widthSum = 0;
+					navList.find("> li").each(function(){
+						widthSum += $(this).outerWidth();
+					});
+					navList.width(widthSum + 30);
+					if(callback != undefined){
+						callback();
+					};
+					_isNavChanged = true;
+				}else if($(window).width() > 801 && _isNavChanged){
+					navList.unwrap().width("");
+					
+					_isNavChanged = false;
+				};
+			};
+			var findPosition = function(){
+				var selectedList = navList.find("> li.on");
+				navList.parent().scrollLeft(selectedList.position().left - 50);
+			};
+			$(window).resize(function(){
+				render();
+			});
+			$(document).ready(function(){
+				render(findPosition);
+			});
+		}
+	};
+	
+	/*
+	 * $.initDough (Dough 페이지 이벤트 일괄적용)
+	 */
+	
+	$.initDough = function(){
+		$("*[data-tip-event]").tip();
+		$(".tab_bar[data-tab-event]").tab();
+		$.initResponsibleNav();
+
+		
+	};
+	
+	
+})(jQuery);
+
+$(document).ready(function(){
+	$.initDough();
+})
--- plug-in/responsible/README.txt
+++ plug-in/responsible/README.txt
@@ -1,12 +1,15 @@
-Responsible Module for Dough v 0.1.1 (Webstandards Tech team, Daum Communications)
+Responsible Module for Dough v 0.2.0 
+(Daum Communicatinos 웹표준기술팀)
 
 ## Update
 
+	Ver 0.2.0 - Dough 0.2.0 버젼에 맞춤 (2012.1.18)
 	Ver 0.1.1 - 최초 배포버젼 (2012.1.18)
 
 ## Dependancy
 
-	Dough 0.1.1 (Core)
+	Dough 0.2.0 Core
+	(dough script 0.2.0 로드 시 Top Navigation 튜닝 효과)
 
 ## Compatibility
 
@@ -16,10 +19,11 @@ Responsible Module for Dough v 0.1.1 (Webstandards Tech team, Daum Communication
 
 ## Usage
 
-	1. Dough 0.1.1 Core와 함께 이 CSS 모듈을 로드시킵니다
+	1. Dough Core와 함께 이 CSS 모듈을 로드시킵니다
 	2. <body> 태그에 .responsible 클래스를 추가합니다.
 
 ## Global Class
 	PC <‐> Mobile 간 레이아웃 변환 최적화를 위해, 본 모듈에서는 다음과 같은 전역클래스를 제공합니다.
+	* .response_hide : 모바일 스크린으로 변환되었을 때 숨김 처리합니다.
 	* .response_no_float : float:left 또는 float:right 속성을 가진 요소에 이 클래스를 줄 경우, 모바일 스크린으로 변환되었을 때 Float를 해제시켜 줍니다.
 	* .response_no_grid : Grid 클래스 등으로 폭(Width) 퍼센티지를 가진 요소에 이 클래스를 줄 경우, 모바일 스크린으로 변환되었을 때 Width가 Auto로 초기화됩니다.
